f5b597139b690c930385f8833f1db64a319a505d
Eugenia Fen <95513839+EugeneFen@users.noreply.github.com>
2022-12-16 20:47:48 +1000

    Add files via upload

---
+++ Chomsky normal form/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Chomsky_normal_form
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grammar a = new Grammar();
+            a.Start_metod();
+            a.Write_Grammar();
+        }
+    }
+}
