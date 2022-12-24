1d82aad31ef566dc2a9f6610b09b1f33c95170b3
Eugenia Fen <95513839+EugeneFen@users.noreply.github.com>
2022-12-24 19:36:27 +1000

    Add files via upload

---
+++ MiniStringFuck/MiniStringFuck/Interpreter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniStringFuck
+{
+    //лишь одна ячейка памяти
+    //счетчик может только увеличиваться
+    //если счетчик достигнет 256, то он обнуляется
+    class Interpreter
+    {
+        private int pointer; //Ячейка памяти
+        private char[] code; //входная строка
+        private string output;
+
+        public Interpreter(string code)
+        {
+            this.code = code.ToCharArray(); //преобразовываем строку в массив символов
+            output = "";
+        }
+
+        public string Run()
+        {
+            for (int i = 0; i < code.Length; i++) //просматриваем всю строку
+            {
+                switch (code[i])
+                {
+                    case '+': //увеличивает счетчик
+                        pointer++;
+                        break;
+                    case '.': //выводит символ
+                        output = output + Convert.ToChar(pointer);
+                        break;                        
+                }
+                if (pointer == 256) pointer = 0;//если счетчик достигнет 256, то он обнуляется
+            }
+            return output;
+        }
+    }
+}
---
+++ MiniStringFuck/MiniStringFuck/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MiniStringFuck
+{
+    class Program
+    {
+        static bool getString(string code)
+        {
+            bool check = true;
+            for (int i = 0; i < code.Length; i++) //просматриваем всю строку
+            {
+                switch (code[i])
+                {
+                    case '+':
+                        check = true;
+                        break;
+                    case '.':
+                        check = true;
+                        break;
+                    default:
+                        check = false;
+                        break;
+                }
+                if (!check) return check;
+            }
+            return check;
+        }
+        static void Main(string[] args)
+        {
+            //string code = Console.ReadLine();
+            //Interpreter b;
+            // if (getString(code))
+            //{
+            // b = new Interpreter(code);
+            // Console.WriteLine(b.Run());
+            //}
+            // else Console.WriteLine("Error: string contain invalid characters");
+
+            AvtoTest a = new AvtoTest();
+            Console.WriteLine("Test 1: " + a.Test_1());
+            Console.WriteLine();
+            Console.WriteLine("Test 2: " + a.Test_2());
+        }
+    }
+}
